d5e1cf7d609adcf45c3a17287ccde2a4cf37e33b
yohei.kanehara <yohei.kanehara@vodori.com>
2017-02-17 00:06:05 -0600

    feature/webpack - Added webpack configˆ

---
+++ build/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: "./src/entry.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query: {
+                    presets: ['react']
+                }
+            }
+        ]
+    }
+};
